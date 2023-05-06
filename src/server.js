39cf0870def5067c04c3b52ba11aea0a8700eaa0
luizfernando <luiz-fsilva37@hotmail.com>
2023-05-06 17:53:09 -0300

    Salvando usuários em memória (Headers)

--- src/server.js
+++ src/server.js
@@ -1,14 +1,24 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((req, res) => {
   const { method, url } = req
 
   if (method === 'GET' && url === '/users') {
-    return res.end('Listagem de usuarios')
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
-    return res.end('Criacao de usuarios')
+    users.push({
+      id: 1,
+      name: 'Logan',
+      email: 'logan@mail.com'
+    })
+
+    return res.end('User created')
   }
 
   return res.end('Welcome to API')
