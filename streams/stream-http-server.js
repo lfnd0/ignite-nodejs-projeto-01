c739105026f00caf912ac9af1ce8fdd7bf65cb7a
luizfernando <luiz-fsilva37@hotmail.com>
2023-05-06 19:25:07 -0300

    Consumindo uma stream completa

--- streams/stream-http-server.js
+++ streams/stream-http-server.js
@@ -5,16 +5,24 @@ class InverseNumberStream extends Transform {
   _transform(chunk, encoding, callback) {
     const transformed = Number(chunk.toString()) * -1
 
-    console.log(transformed);
+    console.log(transformed)
 
     callback(null, Buffer.from(String(transformed)))
   }
 }
 
-const server = http.createServer((req, res) => {
-  return req
-    .pipe(new InverseNumberStream())
-    .pipe(res)
-});
+const server = http.createServer(async (req, res) => {
+  const buffers = []
+
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
+
+  const fullStreamContent = Buffer.concat(buffers).toString()
+
+  console.log(fullStreamContent)
+
+  return res.end(fullStreamContent)
+})
 
 server.listen(3334)
